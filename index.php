73b3cab42a20c85163a877940e544ce6467acc4b
Jyotirmoy <subhodas45036@gmail.com>
2022-10-11 12:33:46 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="" srcset="">
+       <h1>Conference Name</h1> 
+    </header>
+    <div class="sponsors">
+        Technical Sponsors:
+        <div>
+            Sponsor 1
+        </div>
+        <div>
+            Sponsor 2
+        </div>
+    </div>
+    <form action="">
+        <div class="container">
+
+            <div class="cl1">
+                <label for="">First Name</label>
+                <input type="text">
+            </div>
+
+            <div class="cl2">
+                <label for="">Last Name</label>
+                <input type="text">
+            </div>
+
+            <div class="cl1">
+                <label for="">Designation</label>
+                <input type="text">
+            </div>
+
+            <div class="cl2">
+                <label for="">Affiliation</label>
+                <input type="text">
+            </div>
+
+            <!-- <div class="cl">
+                <label for="">First Name</label>
+                <input type="text">
+            </div> -->
+
+        </div>
+    </form>
+</body>
+</html>
